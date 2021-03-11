db5136dd0a5f37968592b7ac1f1b4ebacb697040
JasonManason <Jasondemey1995@gmail.com>
2021-03-11 15:52:28 +0100

    execute_sql function works

--- main.py
+++ main.py
@@ -16,15 +16,43 @@ except:
     print('Something went wrong with the database connection')
 
 
-sql = "SELECT * FROM product LIMIT 4"
-product = cur.execute(sql)
+def execute_sql(sql):
+    cur.execute(sql)
+    records = cur.fetchall()
 
-records = cur.fetchall()
+    return records
 
-for row in records:
-    print('product id: ', row[0])
-    print('name: ', row[1])
-    print(f'price:  {row[2]}\n')
+
+query_list = ["SELECT * FROM product LIMIT 4", "SELECT COUNT(*) as count, value from properties WHERE key like 'discount' GROUP BY value ORDER BY count"]
+
+
+def test_sql(records):
+    for row in records:
+        print(f'product id:  {row[0]}')
+        print(f'name: {row[1]}')
+        print(f'price:  {row[2]}\n')
+
+
+test_sql(execute_sql(query_list[0]))
+
+
+# def count_promo_occurences():
+#     sql = "SELECT COUNT(*) as count, value from properties WHERE key like 'discount' GROUP BY value ORDER BY count"
+#     cur.execute(sql)
+#
+#     records = cur.fetchall()
+#
+#     for row in records:
+#         print(row)
+
+# count_promo_occurences()
+
+'''
+To create tables:
+
+cur.execute("CREATE TABLE top_4? (name VARCHAR(255), address VARCHAR(255))")
+
+'''
 
 
 con.commit()
